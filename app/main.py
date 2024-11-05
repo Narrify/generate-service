c566f2c0fc8553322115b5978b70de4908d5bdcd
Frans <frans.trujillo@utec.edu.pe>
2024-11-05 13:58:41 -0500

    add load test and intergrated on CI

--- app/main.py
+++ app/main.py
@@ -25,4 +25,4 @@ app.include_router(generate_router, prefix="/generate")
 app.include_router(prompts_router, prefix="/prompts")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="localhost", port=8001)
